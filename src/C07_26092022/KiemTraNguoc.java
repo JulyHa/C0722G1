04a5981646e05be5ad3589c8c015fe318487a2d5
JulyHa <nguyenthingu3107@gmail.com>
2022-09-26 20:42:34 +0700

    26092022

--- src/C07_26092022/KiemTraNguoc.java
+++ src/C07_26092022/KiemTraNguoc.java
@@ -4,24 +4,23 @@ import java.util.Stack;
 
 public class KiemTraNguoc {
     public static void main(String[] args) {
-        String s = "(((())())()()()))";
+        String s = "(()(()))())";
         Stack<Character> stack = new Stack<>();
-        boolean check = false;
         for (int i=0; i<s.length(); i++){
             if(s.charAt(i) == '('){
                 stack.add(s.charAt(i));
             }
             else {
                 if(stack.isEmpty()){
-                    break;
+                    System.out.println("Biểu thức sai");
+                    return;
                 }
                 else {
                     stack.pop();
                 }
             }
         }
-        if(stack.isEmpty()) check = true;
-        if(check) System.out.println("Biểu thức đúng");
+        if(stack.isEmpty()) System.out.println("Biểu thức đúng");
         else System.out.println("Biểu thức sai");
     }
 }
